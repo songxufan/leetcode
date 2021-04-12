893b2e248b3477693f70e0a1fa2060ed2834aa65
songxufan <songxufan@bytedance.com>
2021-04-12 20:19:04 +0800

    20210412 night Flatten Binary Tree To Linked List

---
+++ src/leetcode/editor/en/FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,106 @@
+//Given the root of a binary tree, flatten the tree into a "linked list": 
+//
+// 
+// The "linked list" should use the same TreeNode class where the right child po
+//inter points to the next node in the list and the left child pointer is always n
+//ull. 
+// The "linked list" should be in the same order as a pre-order traversal of the
+// binary tree. 
+// 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: root = [1,2,5,3,4,null,6]
+//Output: [1,null,2,null,3,null,4,null,5,null,6]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: root = []
+//Output: []
+// 
+//
+// Example 3: 
+//
+// 
+//Input: root = [0]
+//Output: [0]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the tree is in the range [0, 2000]. 
+// -100 <= Node.val <= 100 
+// 
+//
+// 
+//Follow up: Can you flatten the tree in-place (with O(1) extra space)? Related 
+//Topics Tree Depth-first Search 
+// 👍 4065 👎 397
+
+// 2021-04-09 18:41:02
+
+package leetcode.editor.en;
+public class FlattenBinaryTreeToLinkedList {
+    public static void main(String[] args) {
+        Solution solution = new FlattenBinaryTreeToLinkedList().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        // recursively
+        // base case
+        if (root == null) return;
+        flatten(root.left);
+        flatten(root.right);
+        TreeNode temp = root.right;
+        // 被拉平之后的左子树作为新的右子树
+        root.right = root.left;
+        // root的新的左子树为null
+        root.left = null;
+        // 找到新的右子树的末尾，连接上原来的被拉平之后的右子树
+        // 注意：这里要从root开始，因为原来的左子树，现在的右子树可能为null
+        TreeNode p = root;
+        while (p.right != null) {
+            p = p.right;
+        }
+        p.right = temp;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
+
+
